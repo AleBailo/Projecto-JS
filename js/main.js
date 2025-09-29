8f064ba0c06c924e11308f71d4f5ff90bb39d985
Alejandro Bailo <ale_bailo@hotmail.com>
2025-09-29 10:13:39 -0300

    comienzo del proyecto

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Variable: espacio de memoria reservado para información importante de nuestro sitio web.
+
+// Declaración:
+let cliente;
+
+
+//Asignación de variables:
+cliente = "Juan Perez";
+
+cliente = "Alejandro Bailo";
+
+console.log(cliente);
+
+
+//Inicializar variables: 
+let edad = 34;
+
+console.log(edad);
+
+//Const: es una variable que no puede ser modificada.
+
+const nacimiento = 1990;
+
+console.log(nacimiento);
+
+// Otras practicas.
+
+const titulo = document.getElementById("titulo"); 
+titulo.textContent = "Modificando el titulo desde JS";
