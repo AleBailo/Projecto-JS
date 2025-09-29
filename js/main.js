50355c5873b61a6f5647e396b363bb2c0afd853a
Alejandro Bailo <ale_bailo@hotmail.com>
2025-09-29 15:48:59 -0300

    primer propuesta de proyecto

--- js/main.js
+++ js/main.js
@@ -3,27 +3,31 @@
 // Declaración:
 let cliente;
 
-
 //Asignación de variables:
-cliente = "Juan Perez";
-
 cliente = "Alejandro Bailo";
-
-console.log(cliente);
-
+console.log("Alumno: " + cliente);
 
 //Inicializar variables: 
 let edad = 34;
-
-console.log(edad);
+console.log("Edad: " + edad);
 
 //Const: es una variable que no puede ser modificada.
-
 const nacimiento = 1990;
-
-console.log(nacimiento);
+console.log("Fecha de nacimiento: " + nacimiento);
 
 // Otras practicas.
-
 const titulo = document.getElementById("titulo"); 
-titulo.textContent = "Modificando el titulo desde JS";
+titulo.textContent = "AlacenaApp | Gestiona tus alimentos";
+
+
+// Gestor de alacena.
+
+let producto = prompt("Ingrese el nombre del producto:");
+let cantidad = prompt("Ingrese la cantidad:");
+let fechaCaducidad = prompt("Ingrese la fecha de vencimiento del producto (DD/MM/AAAA):");
+
+console.log("Producto agregado: " + producto);
+console.log("Cantidad: " + cantidad);
+console.log("Fecha de vencimiento: " + fechaCaducidad);
+
+alert("Producto agregado: " + producto + "\nCantidad: " + cantidad + "\nFecha de vencimiento: " + fechaCaducidad);
